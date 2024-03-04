2c19d2ad16967a45add800270b3c1bcab91421d8
Jaewoo.Cho <djdlzl93@gmail.com>
2024-03-04 18:07:16 +0900

    0304_todo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Header, Footer } from "./components/Header";
+
 function App() {
-  const name = "제성";
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addTodo = () => {
+    const newTodo = { text: inputValue, completed: false };
+    setTodos([...todos, newTodo]);
+    setInputValue("");
+  };
+
+  const handleCheck = (index) => {
+    const newTodos = todos.map((todo, todoIndex) =>
+      index === todoIndex ? { ...todo, completed: !todo.completed } : todo
+    );
+  };
 
   return (
     <>
-      <Header />
-      <div>5555</div>
-      <div>1111142134</div>
-      <h1 className="active">{}</h1>
-      <h1>야 호</h1>
-      <div>안녕하세요 여기는 hyun 입니다. </div>
-      <Footer />
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        placeholer="할 일을 적어주세요."
+      />
+      <button type="submit" onClick={addTodo}>
+        추가
+      </button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleCheck(index)}
+            />
+            {todo.text}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
 
+function buttonAdd() {
+  //
+  //체크박스 + input 아래에 새로운 줄을 추가하며 value 입력
+  //inputValue의 데이터를 나열
+  //기존데이터는 아래로 밀기
+}
 export default App;
